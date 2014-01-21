2d0a399d7bd28727b8a482b98f9b47d3caf01e16
carlos <carlos@marte.inesc-id.pt>
2014-01-21 16:26:42 +0000

    new i2c protocol

--- swtests/i2c/i2c_write_read.c
+++ swtests/i2c/i2c_write_read.c
@@ -8,6 +8,7 @@ char* i2c_write = (char*) 0xa0000001;  //(write byte)
 char* i2c_write_stop = (char*)  0xa0000002; //(write byte + stop)
 char* i2c_read = (char*) 0xa0000003; //read byte
 char* i2c_read_stop = (char*)  0xa0000004; //read byte + stop
+char* i2c_status = (char*)  0xa0000007; //status
 
 unsigned char addr_slave = 0xA0;
 unsigned char addr_mem = 0x00;
@@ -16,33 +17,81 @@ unsigned char data = 0xAA;
 int main(void)
 {
   int i=0;
+  int status;
 
 
 //write
-  *(i2c_base) = addr_slave	;
-  *(i2c_write) = addr_mem; 
+*(i2c_base) = addr_slave;
 do{
-  *(i2c_write) = data++;
-   i++;
+	status =(int) *(i2c_status);
+}while ((status & 2));
+
+*(i2c_write) = addr_mem; 
+
+
+do{
+	status =(int) *(i2c_status);
+	if (!(status & 2)){
+  		*(i2c_write) = data++;
+   		i++;
+	}
 }while(i<=10);
-  *(i2c_write_stop) = data++;
+
+do{
+	status =(int) *(i2c_status);
+}while ((status & 2));
+*(i2c_write_stop) = data++;
+
 
 //read
 i=0;
-  *(i2c_base) = addr_slave; 
 
-  *(i2c_write) = addr_mem;
-
-  *(i2c_base) = addr_slave+1; 
 do{
-  *(i2c_read) = 0xFF;
-  data =  *(i2c_read);
-	i++;
-}while(i<=10);
+	status =(int) *(i2c_status);
+}while ((status & 2));
 
+  *(i2c_base) = addr_slave;
+
+do{
+	status =(int) *(i2c_status);
+}while ((status & 2));
+
+*(i2c_write) = addr_mem;  
+
+do{
+	status =(int) *(i2c_status);
+}while ((status & 2));
+
+*(i2c_base) = addr_slave+1;
+
+do{
+	status =(int) *(i2c_status);
+}while ((status & 2));
+ 
+*(i2c_read) = 0xFF;
+
+do{
+
+  status = *(i2c_status);
+  if(!(status & 2)){
+  	data =  *(i2c_read);
+  	*(i2c_read) = 0xFF;
+	i++;
+  }
+}while(i<10);
+
+
+
+do{
+	status =(int) *(i2c_status);
+}while ((status & 2));
+  data =  *(i2c_read);
   *(i2c_read_stop) = 0x00;
+
+do{
+	status =(int) *(i2c_status);
+}while ((status & 2));
   data =  *(i2c_read_stop);
-  printf(" ,%#08X, \n",data);
 
   return 0;
 }
